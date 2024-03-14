36cc309fdef8239eeff79e269b2e52a474242c24
Natália Carvalho Barros <naticarvalhob@gmail.com>
2024-03-14 10:21:40 -0300

    Adicionando atividade1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,29 @@
+// Soma
+let a = 1
+let b = 2
+let c = a + b
+
+console.log(`Resultado da soma entre ${a} e ${b}: ${c}`)
+
+// Subtração
+let d = 4
+let e = 5
+let f = d - e
+
+console.log(`Resultado da subtração entre ${d} e ${e}: ${f}`)
+
+// Multiplicação
+let g = 6
+let h = 7
+let i = g * h
+
+console.log(`Resultado da multiplicação entre ${g} e ${h}: ${i}`)
+
+// Divisão
+let j = 8
+let k = 9
+let l = j / k
+
+console.log(`Resultado da divisão entre ${j} e ${k}: ${l}`) // template string: dentro da string, coloca as variáveis com operador especial
+
+console.log('Resultado da soma entre ' + a + ' e ' + b + ' = ' + c) // concatenando , o que pode trazer mais erros
---
+++ exemplo/ola_mundo.js
@@ -0,0 +1,20 @@
+console.log('Olá, mundo!')
+
+const j = 1
+console.log(j)
+
+var n = 1
+var n = "3"
+console.log(n)
+
+let i = 2
+i = 3 // apenas atualizou o valor
+// se tentasse digitar let i = 3, não deixaria pois já existe
+console.log(i)
+
+m = 4 
+m = 5 // se não especificar, se comporta como var
+console.log(m)
+
+let k
+console.log(k) // undefined
